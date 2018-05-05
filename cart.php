117c2c52ba09cb9e3b5845fff2eb6027ebc0f2d2
ヌルポ <Le.Cai-1@student.uts.edu.au>
2018-05-05 21:47:42 +1000

    add clear function

--- cart.php
+++ cart.php
@@ -38,7 +38,8 @@ function addCart()
 function clearCart()
 {
     session_destroy();
-    echo "Session clear";
+    header("refresh:3;url=cart.php");
+    die ("Your cart is clear");
 }
 
 if (isset($_GET["method"])) {
@@ -55,16 +56,42 @@ if (isset($_GET["method"])) {
 
     }
 }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Cart</title>
+    	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
+	 crossorigin="anonymous">
 
-if (isset($_SESSION["cart_items"])) {
-    echo '<table class="table">';
-    echo '<tr><th>ID</th><th>name</th><th>unit price</th><th>quantity</th><th>Qty</th></tr>';
-    foreach ($_SESSION["cart_items"] as $item) {
-        echo '<tr>';
-        echo "<td>" . $item["item_id"] . "</td>" . "<td>" . $item["item_name"] . "</td>" . "<td>" . $item["uni_price"] . "</td>" . "<td>" . $item["unit_quantity"] . "</td>" . "<td>" . $item["item_qty"] . "</td>";
-        echo '</tr>';
-    }
-    echo '</table>';
-} else {
-    die("<h1>Please add an item into the cart.</h1>");
-}
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp"
+	 crossorigin="anonymous">
+</head>
+<body>
+    <div class="container-fluid">
+    <?php
+        if (isset($_SESSION["cart_items"])) {
+            echo '<table class="table">';
+            echo '<tr><th>ID</th><th>name</th><th>unit price</th><th>quantity</th><th>Qty</th></tr>';
+            foreach ($_SESSION["cart_items"] as $item) {
+                echo '<tr>';
+                echo "<td>" . $item["item_id"] . "</td>" . "<td>" . $item["item_name"] . "</td>" . "<td>" . $item["uni_price"] . "</td>" . "<td>" . $item["unit_quantity"] . "</td>" . "<td>" . $item["item_qty"] . "</td>";
+                echo '</tr>';
+            }
+            echo '</table>';
+            ?>
+                <a href="cart.php?method=clear" class="btn btn-primary pull-right">Clear Cart</a>
+            <?php
+        } else {
+            die("<h1>Please add an item into the cart.</h1>");
+        }
+    ?>
+    </div>
+</body>
+</html>
+
