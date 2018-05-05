131fc6cf5ef360a05c990184887078334c5cc02c
ヌルポ <episodes27@gmail.com>
2018-05-05 21:04:28 +1000

    add cart

---
+++ cart.php
@@ -0,0 +1,70 @@
+<?php
+session_start();
+function addCart()
+{
+
+    $itemID = null;
+    $itemName = null;
+    $uniPrice = null;
+    $orderQty = null;
+    $unitQuantity = null;
+    $cartItems = array();
+
+    if (isset($_GET["item_id"]) ) {
+
+        $itemID = $_GET["item_id"];
+        $orderQty = $_GET["item_qty"];
+        $itemName = $_GET["item_name"];
+        $uniPrice = $_GET["uni_price"];
+        $unitQuantity = $_GET["unit_quantity"];
+
+        //Add to the cart
+        $newItem = [
+            "item_id" => $itemID,
+            "item_name" => $itemName,
+            "uni_price" => $uniPrice,
+            "item_qty" => $orderQty,
+            "unit_quantity" => $unitQuantity,
+        ];
+        if (isset($_SESSION["cart_items"])) {
+            $cartItems = $_SESSION["cart_items"];
+        }
+        array_push($cartItems, $newItem);
+        $_SESSION["cart_items"] = $cartItems;
+
+    }
+}
+function clearCart()
+{
+    session_destroy();
+    echo "Session clear";
+}
+
+if (isset($_GET["method"])) {
+
+    switch ($_GET["method"]) {
+        case "add":{
+                
+                addCart();
+                break;
+            }
+        case "clear":{
+                clearCart();
+                break;
+            }
+
+    }
+}
+
+if (isset($_SESSION["cart_items"])) {
+    echo '<table class="table">';
+    echo '<tr><th>ID</th><th>name</th><th>unit price</th><th>quantity</th><th>Qty</th></tr>';
+    foreach ($_SESSION["cart_items"] as $item) {
+        echo '<tr>';
+        echo "<td>" . $item["item_id"] . "</td>" . "<td>" . $item["item_name"] . "</td>" . "<td>" . $item["uni_price"] . "</td>" . "<td>" . $item["unit_quantity"] . "</td>" . "<td>" . $item["item_qty"] . "</td>";
+        echo '</tr>';
+    }
+    echo '</table>';
+} else {
+    die("<h1>Please add an item into the cart.</h1>");
+}
--- detail.php
+++ detail.php
@@ -20,18 +20,18 @@
     <?php
 include "getDetail.php";
 
-    $id = null;
-    $data = null;
-    if (isset($_GET["id"])) {
-        $id = $_GET["id"];
-        
-        $data = displayItemDetail($id);
-    } else {
-        die("<h1>Please choose an item.</h1>");
-    }
-    if ($data == null) {
-        die("<h1>Invail data</h1>");
-    }
+$id = null;
+$data = null;
+if (isset($_GET["id"])) {
+    $id = $_GET["id"];
+
+    $data = displayItemDetail($id);
+} else {
+    die("<h1>Please choose an item.</h1>");
+}
+if ($data == null) {
+    die("<h1>Invail data</h1>");
+}
 
 ?>
     <div class="container-fluid">
@@ -51,15 +51,34 @@ include "getDetail.php";
                 <td><?php echo $data["unit_quantity"]; ?></td>
            </tr>
         </table>
-        <form action="#" method="post">
-                <div class="form-group">
-                    <label for="qty">QTY:</label>
-                    <input type="number" class="form-control" id="qty" placeholder="Qty to buy">
-                </div>
-                <input type="submit" class="btn btn-primary pull-right" value="Add to cart"/>
-            </form>
+        <form id="item-form" action="cart.php" method="get">
+            <div class="form-group">
+                <label for="item_qty">QTY:</label>
+                <input name="method" type="hidden" value="add"/>
+                <input name="item_id" type="hidden" value="<?php echo $data["product_id"]; ?>"/>
+                <input name="item_name" type="hidden" value="<?php echo $data["product_name"]; ?>"/>
+                <input name="uni_price" type="hidden" value="<?php echo $data["unit_price"]; ?>"/>
+                <input name="unit_quantity" type="hidden" value="<?php echo $data["unit_quantity"]; ?>"/>
+                <input name="item_qty" type="number" class="form-control" id="qty" value="1" placeholder="Qty to buy">
+            </div>
+            <input type="submit" class="btn btn-primary pull-right" value="Add to cart"/>
+        </form>
 
     </div>
+    <script src="https://code.jquery.com/jquery-2.2.4.min.js" type="text/javascript"></script>
 
+    <script>
+        $('#item-form').submit(function (e) {
+            e.preventDefault();
+            $.ajax({
+                url:'cart.php',
+                type:'get',
+                data:$('#item-form').serialize(),
+                success:function(){
+                    alert("add item seccuss");
+                }
+            });
+        });
+    </script>
 </body>
 </html>
